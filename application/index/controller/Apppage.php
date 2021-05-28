715e4cc6bf127f2b15ec136c5ebbc10a25885bfe
zzh <you@example.com>
2021-05-28 15:17:26 +0800

    提交

--- application/index/controller/Apppage.php
+++ application/index/controller/Apppage.php
@@ -62,13 +62,54 @@ class Apppage extends Controller
 
             "createTime"=>date('Y-m-d H:i:s', time()),
             "createUser" => $user['id'],
+            "delflag"=>0,
         );
         $res = $dbadd->addtable("my_expiretime",$data);
         if($res){
             return $dbadd->returnSuss('操作成功！',$res, $data);
         }
     }
+    /**
+     * 编辑
+     */
+    public function editonedata(Request $request){
+        $dbadd = new Dbconllection;
+        $adddata = $request->param();
 
+        $user = session('user');
+        $data= array(
+            "res" =>$adddata['name'],
+            "type" =>$adddata['type'],
+            "img" =>implode(",",$adddata['img']),
+
+            "remarks" =>$adddata['remarks'],
+            "expireDate" =>$adddata['expireDate'],
+
+            "createTime"=>date('Y-m-d H:i:s', time()),
+            "createUser" => $user['id'],
+            "delflag"=>0,
+        );
+        $res = $dbadd->uptable("my_expiretime",array("id"=>$adddata['id']),$data);
+        if($res){
+            return $dbadd->returnSuss('操作成功！',$res, $data);
+        }
+    }
+    /**
+     * 单条查询
+     */
+    public function selectOnedata(Request $request){
+        $dbadd = new Dbconllection;
+        header('Content-Type:application/json; charset=utf-8');
+        header("Access-Control-Allow-Origin:*");
+        header("Access-Control-Allow-Methods:GET");
+        $adddata = $request->param();
+        $user = session('user');
+        $taskdata = $dbadd->selecttable('my_expiretime',array("id"=>$adddata['id'],"createUser"=>$user['id'],"delflag"=>0) );
+        return $dbadd->returnSuss('请求成功！',$taskdata,null);
+    }
+    /**
+     * 列表查询
+     */
     public function querydata(Request $request){
         $dbadd = new Dbconllection;
         header('Content-Type:application/json; charset=utf-8');
@@ -76,8 +117,28 @@ class Apppage extends Controller
         header("Access-Control-Allow-Methods:GET");
         $adddata = $request->param();
         $user = session('user');
-        $taskdata = $dbadd->selecttable('my_expiretime',array("createUser"=>$user['id']) );
+        $taskdata = $dbadd->selecttable('my_expiretime',array("createUser"=>$user['id'],"delflag"=>0) );
         return $dbadd->returnSuss('请求成功！',$taskdata,null);
+    }
+    /**
+     * 删除单条记录
+     * */
+    public function deleteTask(Request $request){
+        $dbadd = new Dbconllection;
+        header('Content-Type:application/json; charset=utf-8');
+        header("Access-Control-Allow-Origin:*");
+        header("Access-Control-Allow-Methods:POST");
+        $adddata = $request->param();
+        $user = session('user');
+
+        $data= array(
+            "delflag" => 1,
+        );
+        $res = $dbadd->uptable("my_expiretime",array("id"=>$adddata['id'],'createUser'=>$user['id']),$data);
+        if($res){
+            return $dbadd->returnSuss('操作成功！',$res,$request->param());
+        }
+
     }
 
 
